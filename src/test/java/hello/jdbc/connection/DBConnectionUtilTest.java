b237e616dd38e2c116e409a4cea7bc4732b698a1
km1031kim <km1031kim@naver.com>
2026-03-19 21:00:16 +0900

    connection pool, datasource

---
+++ src/test/java/hello/jdbc/connection/DBConnectionUtilTest.java
@@ -0,0 +1,20 @@
+package hello.jdbc.connection;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+import static org.assertj.core.api.Assertions.*;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+class DBConnectionUtilTest {
+
+    @Test
+    void connection() {
+        Connection connection = DBConnectionUtil.getConnection();
+        assertThat(connection).isNotNull();
+
+    }
+}
